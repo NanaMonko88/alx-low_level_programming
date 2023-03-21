9f2c63f561f8702b4614389ef3678bbf68e13283
Nana <nanatwumbarimah2013@gmail.com>
2023-03-20 19:08:55 -0700

    PUTS

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - accepts no parameters and returns an integer
+ *
+ * Return: statement to return the integer value
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
