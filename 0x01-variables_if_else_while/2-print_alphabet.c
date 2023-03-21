f9ceba251bba895701d9b3db12fc84293bfee513
Nana <nanatwumbarimah2013@gmail.com>
2023-03-20 21:37:15 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - accepts no parameters and returns an integer
+ *
+ * Return: statement to return the integer value
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+		return (0);
+}
