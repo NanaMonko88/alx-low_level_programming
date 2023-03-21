4bb97c5761cdd1aece9773cb384d3ab18c6e5881
Nana <nanatwumbarimah2013@gmail.com>
2023-03-20 19:15:56 -0700

    PRINTF

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - accepts no parameters and returns an integer
+ *
+ * Return: statement to return the integer value
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
