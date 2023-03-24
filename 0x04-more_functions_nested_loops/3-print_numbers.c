a8c3dc4e545ea2f07a633f4f2c60e76ee9b9e913
Nana <nanatwumbarimah2013@gmail.com>
2023-03-23 19:16:15 -0700

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0 - 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+for (c = '0'; c <= '9'; c++)
+{
+	_putchar(c);
+}
+_putchar("\n");
+}
